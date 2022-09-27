4fc62e95d4784008e73d8a3a382196482955369f
siki2-uiuc <siki2@illinois.edu>
2022-09-27 20:09:37 +0000

    chapter complete

--- array_min_max_difference.rb
+++ array_min_max_difference.rb
@@ -7,3 +7,16 @@
 #   difference
 
 array = [12, 23, 41, 73, 19, 6]
+
+lowest_number = array.min
+
+highest_number = array.max
+
+difference = highest_number - lowest_number
+
+p lowest_number
+p highest_number
+p difference
+
+
+
